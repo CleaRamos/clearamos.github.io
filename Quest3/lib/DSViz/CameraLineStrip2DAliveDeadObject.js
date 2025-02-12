758e1063716d3aaf8d6218b39fd7c6201da48246
Clea Ramos <cjr031@bucknell.edu>
2025-02-12 18:11:34 -0500

    obfuscated quest3

--- Quest3/lib/DSViz/CameraLineStrip2DAliveDeadObject.js
+++ Quest3/lib/DSViz/CameraLineStrip2DAliveDeadObject.js
@@ -21,7 +21,7 @@
  *                                anything the license permits.
  */
 
-import SceneObject from "/lib/DSViz/SceneObject.js"
+import SceneObject from "/Quest3/lib/DSViz/SceneObject.js"
 
 export default class CameraLineStrip2DAliveDeadObject extends SceneObject {
   constructor(device, canvasFormat, cameraPose, vertices) {
@@ -111,7 +111,7 @@ export default class CameraLineStrip2DAliveDeadObject extends SceneObject {
   }
 
   async createShaders() {
-    let shaderCode = await this.loadShader("/shaders/camera2dalivedead.wgsl");
+    let shaderCode = await this.loadShader("/Quest3/shaders/camera2dalivedead.wgsl");
     this._shaderModule = this._device.createShaderModule({
       label: " Shader " + this.getName(),
       code: shaderCode,
